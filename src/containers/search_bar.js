328d5126a7c833820403f80a6250652a4faf0e05
Frison B Alexander <fri.b.alex@gmail.com>
2018-03-08 13:35:30 -0500

    wip save

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,17 +6,32 @@ export default class SearchBar extends Component {
         super(props);
 
         this.state = { term: '' }
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event){
+        debugger;
+        event.preventDefault();
     }
 
     render(){
         return (
-            <form className="input-group">
+            <form className="input-group"
+                  onSubmit={this.onFormSubmit}>
                 <input 
                     placeholder="Get a 5-day forcast"
                     className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
                 />
                 <span className="input-group-btn">
-                    <button type="submit" className="btn btn-secondary" >Submit</button>
+                    <button type="submit" 
+                            className="btn btn-secondary">Submit</button>
                 </span>
             </form>
         );
